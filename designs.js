05e0618b2ede9f74ea35bb27a40897e83c2c09e5
Ekaterina Karelova <katekarelova@gmail.com>
2018-02-06 13:57:33 +0300

    bug fixes, everything works, all done

--- designs.js
+++ designs.js
@@ -4,33 +4,31 @@
 // When size is submitted by the user, call makeGrid()
 
 let table = $('#pixel_canvas');
-let chosenColor = $('#colorPicker').val();
 
 function makeGrid() {
   table.children().remove(); //erasure of existing grid
   let newHeight = $('#input_height').val();
   let newWidth = $('#input_width').val();
 //rows
-  for (r=0, r<newHeight, r++) {
+  for (let r=0; r<newHeight; r++) {
     table.append('<tr></tr>');
 //cols
-    for (c=0, c<newWidth, c++) {
+    for (let c=0; c<newWidth; c++) {
         table.children().last().append('<td></td>');
     };
   };
 };
 //preventing reload
-$("input[type='submit']").click(function(event) {
-  event.preventDefault();
-  makeGrid();
+$('#sizePicker').submit(function(event) {
+	event.preventDefault();
+	makeGrid();
 });
 
-
 //coloring on click
-table.on('click', 'td', coloring() {
-		$(this).css('background-color', chosenColor);
+table.on('click', 'td', function() {
+  $(this).css('background-color', $('#colorPicker').val());
 	});
 //removing color on double click
-table.on('dblclick','td', removeColor() {
-   $(this).css('background-color', none)
+table.on('dblclick','td', function() {
+   $(this).css('background-color', '');
 });
